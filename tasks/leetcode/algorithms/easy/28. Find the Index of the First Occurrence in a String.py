5cff5f0901590b1bbddcde55acc9e0e8c9b7e2be
d.mischenko <d.mischenko@digital-sector.ru>
2024-09-25 08:29:58 +0300

    task

--- tasks/leetcode/algorithms/easy/28. Find the Index of the First Occurrence in a String.py
+++ tasks/leetcode/algorithms/easy/28. Find the Index of the First Occurrence in a String.py
@@ -1,6 +1,4 @@
 def str_str(haystack: str, needle: str) -> int:
-    answer = -1
-    count = 0
     if haystack == needle:
         return 0
     for i in range(len(haystack) - len(needle) + 1):
@@ -8,12 +6,3 @@ def str_str(haystack: str, needle: str) -> int:
         if haystack[i: i + len(needle)] == needle:
             return i
     return -1
-
-print(
-    str_str(
-        haystack = "abc",
-        needle = "c"
-    )
-)
-s = 'a'
-print(s[0: 1])
